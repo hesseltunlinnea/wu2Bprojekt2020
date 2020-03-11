4ca2bf75eae7a53044ccdec27e507a405540bbb6
hesseltunlinnea <linnea.hesseltun@elev.ga.ntig.se>
2020-03-11 15:00:15 +0100

    bilder, cover
    jfjrjrjkfr

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-$("#slideshow > div:gt(0)").hide();
+var slideIndex = 0;
+showSlides();
 
-setInterval(function() {
-  $('#slideshow > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-}, 3000);
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
